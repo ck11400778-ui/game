eaba480888825426065ab2dff95e7bda73ee5ea1
ck11400778-ui <ck11400778@gl.ck.tp.edu.tw>
2025-11-02 07:02:49 +0800

    Delete project directory

--- project/core/camera.py
+++
@@ -1,8 +0,0 @@
-class Camera:
-    def __init__(self, w, h):
-        self.x = 0; self.y = 0; self.w = w; self.h = h
-    def follow(self, rect, world_w, world_h):
-        self.x = int(rect.centerx - self.w/2)
-        self.y = int(rect.centery - self.h/2)
-        self.x = max(0, min(self.x, world_w - self.w))
-        self.y = max(0, min(self.y, world_h - self.h))
